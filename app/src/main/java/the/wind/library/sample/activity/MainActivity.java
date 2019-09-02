0c178e55ab16d70c48705d2812e282e889b5c75a
Vu Thai Duy <vuthaiduy1990@gmail.com>
2019-09-02 10:26:35 +0700

    Refs #2 - Remove FloatingActionButton Use leinardi library instead https://github.com/leinardi/FloatingActionButtonSpeedDial

--- app/src/main/java/the/wind/library/sample/activity/MainActivity.java
+++ app/src/main/java/the/wind/library/sample/activity/MainActivity.java
@@ -3,22 +3,14 @@ package the.wind.library.sample.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import the.wind.library.CWFloatingActionMenu;
 import the.wind.library.sample.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    private CWFloatingActionMenu _actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // init floating action menu
-        _actionMenu = findViewById(R.id.fbActionMenu);
-        _actionMenu.newMenuItem("setting", R.string.app_name, R.mipmap.ic_launcher);
-        _actionMenu.newMenuItem("checklist", R.string.app_name, R.mipmap.ic_launcher);
     }
 
 
