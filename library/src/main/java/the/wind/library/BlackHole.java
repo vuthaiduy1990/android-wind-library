d0b83a06449658b685d34dd58c3db0757a4e219e
Vu Thai Duy <vuthaiduy1990@gmail.com>
2019-10-07 00:22:07 +0700

    Refs #7 - Create Black Hole
    * The center of universe.
    * You can throw everything into blackhole and also pull everything from i

---
+++ library/src/main/java/the/wind/library/BlackHole.java
@@ -0,0 +1,55 @@
+package the.wind.library;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * The center of universe.
+ * You can throw everything into blackhole and also pull everything from it
+ */
+public final class BlackHole {
+
+    // singleton instance of blackhole
+    public static final BlackHole $ = new BlackHole();
+
+    // garbage of all universal
+    private Map<Class<?>, Object> mGarbages = new HashMap<>();
+
+    /* ---------------------- OVERRIDE ----------------------- */
+
+    /* ---------------------- STATIC ------------------------- */
+
+    /* ---------------------- EVENT -------------------------- */
+
+    /* ---------------------- GET-SET ------------------------ */
+
+    /* ---------------------- METHOD ------------------------- */
+
+    /**
+     * Pull garbage from black hole
+     *
+     * @param clazz type of garbage
+     * @param <T>   template type
+     * @return garbage
+     */
+    @SuppressWarnings("unchecked")
+    @Nullable
+    public <T> T pull(@NonNull Class<T> clazz) {
+        return (T) mGarbages.get(clazz);
+    }
+
+    /**
+     * Swallow garbage
+     * You can throw everything into blackhole but don't throw yourself
+     *
+     * @param garbage garbage
+     */
+    public void swallow(Object garbage) {
+        if (garbage == null) return;
+        mGarbages.put(garbage.getClass(), garbage);
+    }
+
+}
