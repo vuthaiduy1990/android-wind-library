53e1775a6c078cdf01e304bc1f5584391ffb450a
Vu Thai Duy <vuthaiduy1990@gmail.com>
2020-01-29 00:23:06 +0700

    refs #9 - [AppDemo]: Init base source code for app demo
    * Add navigation bar (drawer)
    * Add action menu
    * Add page for demoing CheckboxView

--- app/src/main/java/the/wind/library/sample/activity/MainActivity.java
+++ app/src/main/java/the/wind/library/sample/activity/MainActivity.java
@@ -1,17 +1,74 @@
 package the.wind.library.sample.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import the.wind.library.sample.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int[] NAVIGATION_ITEMS = new int[]{
+            R.id._navCheckboxView,
+            R.id._navHome
+    };
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create toolbar
+        Toolbar toolbar = findViewById(R.id._toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create drawer
+        DrawerLayout drawer = findViewById(R.id._drawerLayout);
+        NavigationView navigationView = findViewById(R.id._navigationView);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(NAVIGATION_ITEMS)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id._navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id._navHostFragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_action_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, R.string.action_menu_settings, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/the/wind/library/sample/activity/fragment/HomePage.java
@@ -0,0 +1,25 @@
+package the.wind.library.sample.activity.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import the.wind.library.sample.R;
+
+public class HomePage extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
