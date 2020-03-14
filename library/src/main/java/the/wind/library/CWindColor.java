1753955f15b3e2284958d32f969ae17b2f1bdce2
Vu Thai Duy <vuthaiduy1990@gmail.com>
2020-03-14 21:49:41 +0700

    refs #13 - [WindDialog]: adjust dimension

--- library/src/main/java/the/wind/library/CWindColor.java
+++ library/src/main/java/the/wind/library/CWindColor.java
@@ -15,7 +15,7 @@ public class CWindColor {
     public static final CWindColor SUCCESS = CWindColor.fromHex("#41b3a3");
     public static final CWindColor INFO = CWindColor.fromHex("#30a3b2");
     public static final CWindColor HIGHLIGHT = CWindColor.fromHex("#ecd06f");
-    public static final CWindColor WARNING = CWindColor.fromHex("#ffcc00");
+    public static final CWindColor WARNING = CWindColor.fromHex("#f4bb1a");
     public static final CWindColor DANGER = CWindColor.fromHex("#e26060");
     public static final CWindColor NEUTRAL = CWindColor.fromHex("#e8a87c");
     public static final CWindColor LIGHT = CWindColor.fromHex("#dfdfde");
