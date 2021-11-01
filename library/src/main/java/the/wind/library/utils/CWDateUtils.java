87ece2e36227875ad8aa696fd9086680643b72d6
Vu Thai Duy <vuthaiduy1990@gmail.com>
2021-11-01 23:18:40 +0700

    refs #22 - [Calendar]: get start and end of day

--- library/src/main/java/the/wind/library/utils/CWDateUtils.java
+++ library/src/main/java/the/wind/library/utils/CWDateUtils.java
@@ -82,6 +82,20 @@ public final class CWDateUtils {
     }
 
     /**
+     * Get start of day
+     *
+     * @param cal calendar
+     * @return start of day
+     */
+    public static Date getStartOfDay(Calendar cal) {
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    /**
      * Get end of day
      *
      * @param cal  calendar
@@ -90,6 +104,20 @@ public final class CWDateUtils {
      */
     public static Date getEndOfDay(Calendar cal, Date date) {
         cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
+        return cal.getTime();
+    }
+
+    /**
+     * Get end of day
+     *
+     * @param cal calendar
+     * @return end of day
+     */
+    public static Date getEndOfDay(Calendar cal) {
         cal.set(Calendar.HOUR_OF_DAY, 23);
         cal.set(Calendar.MINUTE, 59);
         cal.set(Calendar.SECOND, 59);
