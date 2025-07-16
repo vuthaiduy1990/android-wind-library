b6d5f46f0b2f4ab984df27ca7e7a8313a4210823
Vu Thai Duy <vuthaiduy1990@gmail.com>
2025-07-16 22:59:57 +0700

    Add bamboo note to home page

--- app/src/main/java/the/wind/library/sample/activity/fragment/HomePage.java
+++ app/src/main/java/the/wind/library/sample/activity/fragment/HomePage.java
@@ -1,9 +1,16 @@
 package the.wind.library.sample.activity.fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +18,9 @@ import androidx.fragment.app.Fragment;
 import the.wind.library.sample.R;
 
 public class HomePage extends Fragment {
+
+    // underline span
+    private final static UnderlineSpan UNDERLINE_SPAN = new UnderlineSpan();
 
     @Nullable
     @Override
@@ -21,5 +31,46 @@ public class HomePage extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        {
+            view.findViewById(R.id._footerText1).setOnClickListener(v -> {
+                openBambooNoteApp(v.getContext());
+            });
+            TextView _footerText2 = view.findViewById(R.id._footerText2);
+            String text1 = getString(R.string.bamboo_note);
+            SpannableString span = new SpannableString(text1);
+            span.setSpan(UNDERLINE_SPAN, 0, text1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            _footerText2.setText(span, TextView.BufferType.NORMAL);
+            _footerText2.setOnClickListener(v -> {
+                openBambooNoteApp(v.getContext());
+            });
+        }
+
+        {
+            View _icon1 = view.findViewById(R.id._icon1);
+            _icon1.setOnClickListener(v -> {
+                openBambooNoteApp(v.getContext());
+            });
+            view.findViewById(R.id._downloadBtn).setOnClickListener(v -> {
+                openBambooNoteApp(v.getContext());
+            });
+        }
+
+    }
+
+    /**
+     * Open this application page on play store
+     *
+     * @param context application context
+     */
+    public static void openBambooNoteApp(Context context) {
+        final String appPackageName = "the.wind.bamboo.note";
+        Uri uri;
+        try {
+            uri = Uri.parse("market://details?id=" + appPackageName);
+        } catch (Exception ex) {
+            uri = Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName);
+        }
+        context.startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 }
